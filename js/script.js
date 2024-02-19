b0aa50a05b921bb2c7e0060db65eb9822154cd5a
hrkk7t <159084440+hrkk7t@users.noreply.github.com>
2024-02-19 20:30:51 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-function toggleNav() {
-  var body = document.body;
-  var hamburger = document.getElementById('js-hamburger');
-  var blackBg = document.getElementById('js-black-bg');
+$(function () {
 
-  hamburger.addEventListener('click', function() {
-    body.classList.toggle('nav-open');
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
   });
-  blackBg.addEventListener('click', function() {
-    body.classList.remove('nav-open');
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
   });
-}
-toggleNav();
+
+});
+
